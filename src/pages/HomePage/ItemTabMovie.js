507aefb8a0a48f4783b4ad65f8c931de3a1fdc10
Quang <quang@Killer-b-MBA.local>
2022-10-14 10:27:05 +0700

    update tabMovie function

--- src/pages/HomePage/ItemTabMovie.js
+++ src/pages/HomePage/ItemTabMovie.js
@@ -1,8 +1,10 @@
 import React from "react";
 import moment from "moment";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function ItemTabMovie({ movie }) {
+  let user = useSelector((state) => state.userReducer.userInfo);
   return (
     <div className="flex space-x-5 border-b border-red-500 p-3">
       <img
@@ -14,8 +16,19 @@ export default function ItemTabMovie({ movie }) {
         <p>{movie.tenPhim}</p>
         <div className="grid grid-cols-3 gap-5">
           {movie.lstLichChieuTheoPhim.slice(0, 6).map((gioChieu, index) => {
-            return (
+            return user ? (
               <NavLink key={index} to={`/purchase/${gioChieu.maLichChieu}`}>
+                <div
+                  type="button"
+                  className="p-3 rounded bg-red-600 text-white"
+                >
+                  {moment(gioChieu.ngayChieuGioChieu).format(
+                    "DD/MM/YYYY - hh:mm"
+                  )}
+                </div>
+              </NavLink>
+            ) : (
+              <NavLink key={index} to="/login">
                 <div
                   type="button"
                   className="p-3 rounded bg-red-600 text-white"
